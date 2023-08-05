860a8e3f793337310058128c28b51f76ee546fb4
ShayersUtsho <s.ssutsho.ch@gmail.com>
2023-08-05 21:43:14 +0600

    Completed

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -9,8 +12,11 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] private KeyCode down = KeyCode.DownArrow;
     [SerializeField] private KeyCode left = KeyCode.LeftArrow;
     [SerializeField] private KeyCode right = KeyCode.RightArrow;
+    public bool win = false;
     private Rigidbody rb;
     private bool inTrigger = false;
+    [SerializeField] private Transform winMessage;
+    [SerializeField] private float winMessageMoveDuration = 0f;
     // Start is called before the first frame update
     void Awake()
     {
@@ -25,16 +31,24 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(right))
-            rb.MovePosition(rb.position + Vector3.forward * moveSpeed * Time.deltaTime);
-        if (Input.GetKey(left))
-            rb.MovePosition(rb.position + Vector3.back * moveSpeed * Time.deltaTime);
-        if (inTrigger)
+        if (!win)
         {
-            if (Input.GetKey(up))
-                rb.MovePosition(rb.position + Vector3.up * moveSpeed * 2f * Time.deltaTime);
-            if (Input.GetKey(down))
-                rb.MovePosition(rb.position + Vector3.down * moveSpeed * 0.5f * Time.deltaTime);
+            if (Input.GetKey(right))
+                rb.MovePosition(rb.position + Vector3.forward * moveSpeed * Time.deltaTime);
+            if (Input.GetKey(left))
+                rb.MovePosition(rb.position + Vector3.back * moveSpeed * Time.deltaTime);
+            if (inTrigger)
+            {
+                if (Input.GetKey(up))
+                    rb.MovePosition(rb.position + Vector3.up * moveSpeed * 2f * Time.deltaTime);
+                if (Input.GetKey(down))
+                    rb.MovePosition(rb.position + Vector3.down * moveSpeed * 0.5f * Time.deltaTime);
+            }
+        }
+        else if(winMessageMoveDuration < 1000f)
+        {
+            winMessage.Translate(Vector3.right * Time.deltaTime);
+            winMessageMoveDuration += 1f;
         }
     }
 
@@ -43,11 +57,15 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (other.CompareTag("Updraft"))
             inTrigger = true;
+        if (other.CompareTag("Goal"))
+            win = true;
     }
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Updraft"))
             inTrigger = true;
+        if (other.CompareTag("Goal"))
+            win = true;
     }
     private void OnTriggerExit(Collider other)
     {
